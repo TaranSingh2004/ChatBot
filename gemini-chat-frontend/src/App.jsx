d35a76a250e6cd5437e0e6da61029e868990d9e6
TaranSingh2004 <taranduggal2004@gmail.com>
2025-03-09 10:22:11 +0530

    chatbot frontend and webconfig

--- gemini-chat-frontend/src/App.jsx
+++ gemini-chat-frontend/src/App.jsx
@@ -1,16 +1,41 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import ChatInput from './components/ChatInput'
+import ChatResponse from './components/ChatResponse'
+import { fetchChatResponse } from './services/api';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [response, setResponse] = useState(null);
+  const [loading, setLoading] = useState(false);
+
+  const handleQuestionSubmit = async (question) => {
+    setLoading(true);
+    setResponse(null);
+    try {
+      const apiResponse = await fetchChatResponse(question);
+      setResponse(apiResponse);
+    } catch (error) {
+      alert("Failed to get response")
+    } finally {
+      setLoading(false);
+    }
+  }
+
+  
+  // if (loading) {
+  //   return (<h1>Loading...</h1>);
+  // }
 
   return (
-    <>
-      gunit khassad
-      <button type="button" class="btn btn-warning">yes</button>
-    </>
+    <div className='App'>
+      <header className='bg-primary text-white text-center py-4'>
+        <h1>Gemini ChatBot</h1>
+      </header>
+      <ChatInput onSubmit={handleQuestionSubmit}/>
+      {loading &&  <h3>Loading...</h3>}
+        <ChatResponse response={response} />
+      {/* RESPONSE */}
+    </div>
   )
 }
 
